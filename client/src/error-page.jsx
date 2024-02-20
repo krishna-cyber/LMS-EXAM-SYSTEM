6f26b94dcc25f81d735d84c9f22334263b885269
krishna-cyber <tiwarikrishna54321@gmail.com>
2024-02-20 09:01:35 +0545

    error page navigate setup

--- client/src/error-page.jsx
+++ client/src/error-page.jsx
@@ -1,18 +1,47 @@
 /** @format */
 
 import { useRouteError } from "react-router-dom";
+import { Button } from "antd";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id='error-page'>
+    <div
+      className=' flex justify-center items-center gap-4 flex-col h-screen w-screen'
+      id='error-page'>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <div className='buttons flex gap-3 w-[20%] justify-between'>
+        <Button
+          className=' font-semibold bg-red-700'
+          type='primary'
+          onClick={() => {
+            window.location.reload();
+          }}>
+          Refresh
+        </Button>
+        <Button
+          className=' bg-green-700 font-semibold'
+          type='primary'
+          onClick={() => {
+            window.location.assign("/");
+          }}>
+          Go Home
+        </Button>
+        <Button
+          className='bg-blue-700 font-semibold'
+          type='primary'
+          onClick={() => {
+            window.history.back();
+          }}>
+          Go Back
+        </Button>
+      </div>
     </div>
   );
 }
